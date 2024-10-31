ca5ab06b3d71e6aeb0c30cd16e3d8eb08992adaa
julissa Bernal <152914827+JulissaBernal@users.noreply.github.com>
2024-10-31 14:07:06 -0600

    Positive dominance in array ready

---
+++ positiveArray.js
@@ -0,0 +1,17 @@
+// Write a function to determine if an array is positively dominant.
+// An array is positively dominant when the majority of its elements are positive.
+ 
+// Example:
+// - `positiveDom([-1, -3, -5, 4, 6767])` should return `false`.
+
+const positiDominant=(array)=>{
+let arrayNegativos=array.filter((number)=> number < 0)
+if(arrayNegativos.length > array.length/2){
+   console.log("true")
+}else{
+    console.log("false")
+}
+return;
+}
+
+positiDominant([-1, -3, -5, 4, 6767]);
